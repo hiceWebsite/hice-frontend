d6e9e8c0bf99d0dd58da052c3865f6b2be2fc6be
abdullah-mamun369 <maamamun369@gmail.com>
2025-08-14 18:53:58 +0600

    test change for api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,12 +1,25 @@
+// import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+// import { createApi } from "@reduxjs/toolkit/query/react";
+// import { tagTypesList } from "../tag-types";
+
+// // Define a service using a base URL and expected endpoints
+// export const baseApi = createApi({
+//   reducerPath: "api",
+//   baseQuery: axiosBaseQuery({ baseUrl: "https://3dmodels.hice.com.au/api/v1" }),
+//   // baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+//   endpoints: () => ({}),
+//   tagTypes: tagTypesList,
+// });
+
 import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { tagTypesList } from "../tag-types";
 
-// Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: axiosBaseQuery({ baseUrl: "https://3dmodels.hice.com.au/api/v1" }),
-  // baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: axiosBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "/api/v1",
+  }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
 });
--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -5,6 +5,7 @@ import setAccessToken from "./setAccessToken";
 
 export const userLogin = async (data: FieldValues) => {
   const res = await fetch("https://3dmodels.hice.com.au/api/v1/auth/login", {
+    //   const res = await fetch("http://localhost:5000/api/v1/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
